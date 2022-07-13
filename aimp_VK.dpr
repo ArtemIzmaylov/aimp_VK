9d2bd91e5887cf7154963d72f6492242d32c5b6c
Keller18306 <kompeter17@gmail.com>
2022-07-14 00:34:19 +0300

    update project files

--- aimp_VK.dpr
+++ aimp_VK.dpr
@@ -9,6 +9,7 @@ uses
   Windows,
   Forms,
   apiPlugin,
+  AIMP.VK.Classes in 'AIMP.VK.Classes.pas',
   AIMP.VK.Core in 'AIMP.VK.Core.pas',
   AIMP.VK.Plugin in 'AIMP.VK.Plugin.pas',
   AIMP.VK.Plugin.DataStorage in 'AIMP.VK.Plugin.DataStorage.pas',
@@ -19,7 +20,7 @@ uses
   AIMP.VK.Plugin.Dialogs.Auth in 'AIMP.VK.Plugin.Dialogs.Auth.pas',
   AIMP.VK.Plugin.Dialogs.Downloader in 'AIMP.VK.Plugin.Dialogs.Downloader.pas' {frmVKDownloader},
   AIMP.VK.Plugin.Dialogs.Lyrics in 'AIMP.VK.Plugin.Dialogs.Lyrics.pas' {frmVKLyrics},
-  AIMP.VK.Plugin.Dialogs.Settings in 'AIMP.VK.Plugin.Dialogs.Settings.pas',
+  AIMP.VK.Plugin.Dialogs.Settings in 'AIMP.VK.Plugin.Dialogs.Settings.pas' {frmVKSettings},
   AIMP.VK.Plugin.SmartPlaylists in 'AIMP.VK.Plugin.SmartPlaylists.pas';
 
   function AIMPPluginGetHeader(out Header: IAIMPPlugin): HRESULT; stdcall;
